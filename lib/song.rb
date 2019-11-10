4521cdad3953edbb973f470ca8a3914b49872d97
Jose Chavira <joeychavira2016@gmail.com>
2019-11-10 21:30:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 class Song
-  extend Memorable:ClassMethods
-  include Memorable:InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
-
+  extend Memorable:ClassMethods
+  include Memorable:InstanceMethods
   @@songs = []
 
   def initialize
